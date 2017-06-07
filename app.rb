c3dc5a003c9d969f62e071034d3fc9111a244a66
github username <github email address>
2017-06-07 17:41:47 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,12 +6,15 @@ class App < Sinatra::Base
     erb :reverse
   end
 
-  post '/reverse' do
+  post '/reverse' do #post is a FORM that exists, so you need a ruby method to correspond with the HTML form
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
     erb :reversed
   end
 
   get '/friends' do
-    #write your code here
-  end
+     @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+     erb :friends
+   end
 
 end
